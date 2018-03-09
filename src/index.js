722f49cbbe2971fc6ee1a92be5b58e6ffdcc8da5
NSpeko <nonstoper42@gmail.com>
2018-03-09 21:20:25 +0300

    Feature: Half-implementation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
+const createEnumerableProperty = (propertyName) => {return {propertyName}};
+const createNotEnumerableProperty = (propertyName) => {
+    if (Object[propertyName]) delete Object.propertyName;
+    Object.defineProperty(Object.prototype, propertyName, {
+        enumerable: false,
+        set: function (x) {
+            Object.prototype.propertyName = x;
+        },
+        get: function () {
+            return Object.prototype.propertyName;
+        }
+    });
+    return propertyName;
+};
+const createProtoMagicObject = () => {return Function;};
 const incrementor = () => {};
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (param) => {
+    return new Promise((resolve) => {setTimeout(()=>{resolve(param);},1001);});
+};
 const getDeepPropertiesCount = () => {};
 const createSerializedObject = () => {};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => {
+    return arr.sort((obj1,obj2)=>{
+        return obj1.__proto__>obj2.__proto__;
+});
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
